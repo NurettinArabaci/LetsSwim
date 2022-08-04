08580a2d516281c06f990eacfbd7b564b0592ff3
NurettinArabaci <nurettinarabaci@gmail.com>
2022-08-04 17:01:46 +0300

    imp Player script

--- Assets/01_Components/Player/Player.cs
+++ Assets/01_Components/Player/Player.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Transform mT;
+    Animator mAnim;
+    Rigidbody rb;
+    int speed = 10;
+
+    public static bool isActiveGame;
+
+    public static bool upMovement;
+
+    private void Awake()
     {
+        mT = transform;
+        mAnim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody>();
+        Physics.gravity = Vector3.zero;
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            mT.position += Vector3.down / 2;
+            upMovement = false;
+            mAnim.SetTrigger("swim");
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            mT.position += (Vector3.forward+Vector3.down/4) * Time.deltaTime * speed;
+            
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            upMovement = true;
+      
+        }
+
+        if (upMovement)
+        {
+            mT.position += (Vector3.forward + Vector3.up / 4) * Time.deltaTime * speed;
+        }
+
     }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+
+            upMovement = false;
+            mAnim.SetTrigger("idle");
+        }
+    }
+    
 }
